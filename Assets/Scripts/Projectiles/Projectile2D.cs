1714e4bb39f9ad052f2bc29dddd2a7d74d140bcf
William Ritson <william.ritson@gmail.com>
2020-05-08 21:08:17 -0400

    Adds projectile system.

---
+++ Assets/Scripts/Projectiles/Projectile2D.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile2D : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public int damage = 1;
+    public Element damageType;
+    
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = transform.right.normalized * speed;
+    }
+
+     void OnTriggerEnter2D(Collider2D col) {
+        Damagable damagable = col.GetComponent<Damagable>();
+        if (damagable) {
+            damagable.TakeDamage(damage, damageType);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Projectiles/ProjectileLauncher.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(Controller))]
+public class ProjectileLauncher : MonoBehaviour
+{
+    public float timeBetweenShots = 0.2f;
+    public GameObject projectile;
+    public GameObject firePoint;
+
+    private Controller controller;
+    private float timeSinceLastShot = 10.0f;
+
+    private void Awake()
+    {
+        controller = GetComponent<Controller>();
+        controller.OnLaunchProjectile += LaunchAtPosition;
+    }
+
+    void Update()
+    {
+        timeSinceLastShot += Time.deltaTime;
+    }
+
+    void LaunchAtPosition(Vector2 target)
+    {
+        if (timeSinceLastShot <= timeBetweenShots) return;
+        Instantiate(projectile, firePoint.transform.position, transform.rotation);
+        timeSinceLastShot = 0.0f;
+    }
+}
