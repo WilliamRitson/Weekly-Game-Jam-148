d4ff4e661c9dc5708476348b8d2668063b8fbd82
GHZaher <53429565+GHZaher@users.noreply.github.com>
2020-05-09 04:44:09 +0300

    fix #1

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rig;
+    private Controller controller;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        controller = GetComponent<Controller>();
+    }
+
+    private void FixedUpdate()
+    {
+        rig.velocity = controller.movementDirection * speed * Time.fixedDeltaTime;
+    }
+}
