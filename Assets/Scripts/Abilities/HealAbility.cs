d0335b672f2572f36f87168696d5aea577635994
William Ritson <william.ritson@gmail.com>
2020-05-09 21:09:51 -0400

    Adds heal ability

---
+++ Assets/Scripts/Abilities/HealAbility.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Damagable))]
+public class HealAbility : Ability
+{
+    public int healAmount = 3;
+
+    Damagable dmg;
+    private void Awake()
+    {
+        dmg = GetComponent<Damagable>();
+    }
+
+    protected override void ActivateAbility(Vector2 target)
+    {
+        dmg.SetHealth(dmg.CurrentLife + healAmount);
+    }
+
+    public override bool ShouldUse(GameObject target)
+    {
+        return (dmg.MaximumLife - dmg.CurrentLife) <= healAmount;
+    }
+}
