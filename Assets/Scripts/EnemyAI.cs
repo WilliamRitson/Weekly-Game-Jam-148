12ab0ea6818b4baf354ab532271defd256bfeb67
thenerdpoint60 <thenerdpoint60@gmail.com>
2020-05-10 16:23:09 +0530

    Basic Enemy FUnctionality

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,27 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class EnemyAI : Controller
 {
+    public float moveSpeed = 5f;
+
+
+
     GameObject player;
     private float maxEngagmentDist = 8;
     private float squaredMaxEngagmentDist;
 
+    //private Vector3 startingPosition;
+    Vector3 playerPos;
+    Vector2 movement;
+
+    private Rigidbody2D rb;
+
     private void Awake()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         squaredMaxEngagmentDist = maxEngagmentDist * maxEngagmentDist;
+        //startingPosition = transform.position;
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        if (player == null)
-        {
-            player = GameObject.FindGameObjectWithTag("Player");
-            if (!player) return;
-        }
-        Vector3 playerPos = player.transform.position;
+        playerPos = player.transform.position;
         if ((transform.position - playerPos).sqrMagnitude > squaredMaxEngagmentDist)
         {
             movementDirection = Vector3.zero;
@@ -29,8 +42,33 @@ public class EnemyAI : Controller
         }
 
         movementDirection = playerPos - transform.position;
+        float angle = Mathf.Atan2(movementDirection.y, movementDirection.x)*Mathf.Rad2Deg;
+        rb.rotation = angle;
+        movementDirection.Normalize();
+        movement = movementDirection;
+
         TriggerProjectileAttack(playerPos);
     }
+
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (moveSpeed * direction * Time.deltaTime));
+    }
+
+
+
+    //private Vector3 PatrollingPosition()
+    //{
+    //    Vector3 randomDir = new Vector3(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f)).normalized;
+    //    return startingPosition + randomDir * UnityEngine.Random.Range(10f, 50f); 
+    //}
+
+
+
 }
 
 
