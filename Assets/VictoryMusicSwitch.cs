514b143f5d49dbd2dedf3a373e359bdda0aad0be
joshuaragoodman <61438305+joshuaragoodman@users.noreply.github.com>
2020-05-14 10:41:34 -0400

    VictoryMusic added to victory scene and audio manager

---
+++ Assets/VictoryMusicSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryMusicSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.SharedInstance().StopAllMusic();
+        AudioManager.SharedInstance().PlayVictoryMusic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
