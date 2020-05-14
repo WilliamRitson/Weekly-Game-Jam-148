90cf0a06535db0f71b738ab33038fe273362bdcd
joshuaragoodman <61438305+joshuaragoodman@users.noreply.github.com>
2020-05-13 21:55:36 -0400

    Music bug fix

--- Assets/Scripts/SwitchMusicTrigger.cs
+++ Assets/Scripts/SwitchMusicTrigger.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿/*
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -26,3 +27,4 @@ public class SwitchMusicTrigger : MonoBehaviour
         }
    // }
 }
+*/
