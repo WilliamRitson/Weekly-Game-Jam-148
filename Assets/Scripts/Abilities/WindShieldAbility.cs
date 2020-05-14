dfb523350bf8c44df6abe52bdae97875573a2495
GHZaher <53429565+GHZaher@users.noreply.github.com>
2020-05-14 17:21:07 +0300

    King Animation And wind shield

--- Assets/Scripts/Abilities/WindShieldAbility.cs
+++ Assets/Scripts/Abilities/WindShieldAbility.cs
@@ -6,9 +6,16 @@ public class WindShieldAbility : Ability
 {
     public float abilityDuration;
     public ProjectileSquander projectileSquander;
+    public GameObject  WindShield;
 
     private void Start()
     {
+        if (WindShield == null)
+        {
+            WindShield = transform.GetChild(1).gameObject;
+        }
+        WindShield.SetActive(false);
+
         if (projectileSquander == null)
         {
             projectileSquander = transform.GetChild(0).GetComponent<ProjectileSquander>();
@@ -27,9 +34,16 @@ public class WindShieldAbility : Ability
 
     public IEnumerator DeactivateAbility()
     {
+        if (WindShield == null)
+        {
+            WindShield = transform.GetChild(1).gameObject;
+        }
+
+        WindShield.SetActive(true);
         AudioManager.SharedInstance().PlayWindShieldAudio();
         projectileSquander.gameObject.SetActive(true);
         yield return new WaitForSeconds(abilityDuration);
         projectileSquander.gameObject.SetActive(false);
+        WindShield.SetActive(false);
     }
 }
